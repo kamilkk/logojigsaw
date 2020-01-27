cb441d14b0a802f32d3c6f0562f58bf0f814990f
Kamil Kowalski <kamil.k.kowalski@gmail.com>
2020-01-27 13:16:57 +0100

    Login page changes: - login page created and added - router created and added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import LoginPage from './Pages/login-page';
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/" component={LoginPage} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
